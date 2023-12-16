8ac5ca1c90cdf1158099b47064be2b98542ee815
fevse <fedulov.v.s@gmail.com>
2023-12-16 15:24:00 +0400

    HW03 is completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,41 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+func Top10(s string) []string {
+	ms := make(map[string]int)
+	sl := strings.Fields(s)
+	for _, v := range sl {
+		ms[v]++
+	}
+	res := mapToSortSlice(ms)
+	if len(res) > 10 {
+		return res[:10]
+	}
+	return res
+}
+
+func mapToSortSlice(m map[string]int) []string {
+	type keyValue struct {
+		Key   string
+		Value int
+	}
+	skv := make([]keyValue, 0, len(m))
+	for k, v := range m {
+		skv = append(skv, keyValue{k, v})
+	}
+	sort.Slice(skv, func(i, j int) bool {
+		if skv[i].Value == skv[j].Value {
+			return skv[i].Key < skv[j].Key
+		}
+		return skv[i].Value > skv[j].Value
+	})
+	res := make([]string, 0, len(skv))
+	for _, v := range skv {
+		res = append(res, v.Key)
+	}
+	return res
 }
