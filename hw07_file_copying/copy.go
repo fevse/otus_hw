14ba9e668d1e9716cd68612abf1f15d43ff630f7
fevse <fedulov.v.s@gmail.com>
2024-02-03 17:32:22 +0400

    HW07 is completed

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
@@ -10,6 +14,62 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	mode, err := os.Lstat(fromPath)
+	if err != nil {
+		return err
+	}
+	if !mode.Mode().IsRegular() {
+		return ErrUnsupportedFile
+	}
+
+	rfile, err := os.Open(fromPath)
+	if err != nil {
+		return err
+	}
+
+	s, err := rfile.Stat()
+	if err != nil {
+		return err
+	}
+
+	if s.Size() < offset {
+		return ErrOffsetExceedsFileSize
+	}
+
+	wfile, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+
+	if limit == 0 {
+		limit = s.Size() - offset
+	} else if limit > s.Size()-offset {
+		limit = s.Size() - offset
+	}
+
+	b := make([]byte, 1)
+
+	rfile.Seek(offset, io.SeekStart)
+	var n int64
+
+	bar := pb.New(int(limit))
+	bar.Start()
+
+	reader := bar.NewProxyReader(rfile)
+
+	for n < limit {
+		read, err := reader.Read(b)
+		n += int64(read)
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			return err
+		}
+		wfile.Write(b)
+	}
+	bar.Finish()
+	wfile.Close()
+
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,174 @@
 package main
 
-import "testing"
+import (
+	"log"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	t.Run("error unsupported file", func(t *testing.T) {
+		r, err := os.CreateTemp("testdata", "read")
+		if err != nil {
+			log.Fatal(err)
+		}
+		w, err := os.CreateTemp("testdata", "write")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer os.Remove(w.Name())
+		defer os.Remove(r.Name())
+
+		Copy("testdata", w.Name(), 0, 0)
+		require.Error(t, ErrUnsupportedFile)
+	})
+	t.Run("error offset exceeds file size", func(t *testing.T) {
+		r, err := os.CreateTemp("testdata", "read")
+		if err != nil {
+			log.Fatal(err)
+		}
+		w, err := os.CreateTemp("testdata", "write")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer os.Remove(w.Name())
+		defer os.Remove(r.Name())
+
+		Copy(r.Name(), w.Name(), 1024, 0)
+		require.Error(t, ErrOffsetExceedsFileSize)
+	})
+	t.Run("test data out offset0 limit0", func(t *testing.T) {
+		w, err := os.CreateTemp("testdata", "out")
+		if err != nil {
+			log.Fatal(err)
+		}
+		o, err := os.Open("testdata/out_offset0_limit0.txt")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer os.Remove(w.Name())
+
+		Copy("testdata/input.txt", w.Name(), 0, 0)
+		wStat, err := w.Stat()
+		if err != nil {
+			log.Fatal(err)
+		}
+		oStat, err := o.Stat()
+		if err != nil {
+			log.Fatal(err)
+		}
+		require.Equal(t, wStat.Size(), oStat.Size())
+	})
+	t.Run("test data out offset0 limit10", func(t *testing.T) {
+		w, err := os.CreateTemp("testdata", "out")
+		if err != nil {
+			log.Fatal(err)
+		}
+		o, err := os.Open("testdata/out_offset0_limit10.txt")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer os.Remove(w.Name())
+
+		Copy("testdata/input.txt", w.Name(), 0, 10)
+		wStat, err := w.Stat()
+		if err != nil {
+			log.Fatal(err)
+		}
+		oStat, err := o.Stat()
+		if err != nil {
+			log.Fatal(err)
+		}
+		require.Equal(t, wStat.Size(), oStat.Size())
+	})
+	t.Run("test data out offset0 limit1000", func(t *testing.T) {
+		w, err := os.CreateTemp("testdata", "out")
+		if err != nil {
+			log.Fatal(err)
+		}
+		o, err := os.Open("testdata/out_offset0_limit1000.txt")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer os.Remove(w.Name())
+
+		Copy("testdata/input.txt", w.Name(), 0, 1000)
+		wStat, err := w.Stat()
+		if err != nil {
+			log.Fatal(err)
+		}
+		oStat, err := o.Stat()
+		if err != nil {
+			log.Fatal(err)
+		}
+		require.Equal(t, wStat.Size(), oStat.Size())
+	})
+	t.Run("test data out offset0 limit10000", func(t *testing.T) {
+		w, err := os.CreateTemp("testdata", "out")
+		if err != nil {
+			log.Fatal(err)
+		}
+		o, err := os.Open("testdata/out_offset0_limit10000.txt")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer os.Remove(w.Name())
+
+		Copy("testdata/input.txt", w.Name(), 0, 10000)
+		wStat, err := w.Stat()
+		if err != nil {
+			log.Fatal(err)
+		}
+		oStat, err := o.Stat()
+		if err != nil {
+			log.Fatal(err)
+		}
+		require.Equal(t, wStat.Size(), oStat.Size())
+	})
+	t.Run("test data out offset100 limit1000", func(t *testing.T) {
+		w, err := os.CreateTemp("testdata", "out")
+		if err != nil {
+			log.Fatal(err)
+		}
+		o, err := os.Open("testdata/out_offset100_limit1000.txt")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer os.Remove(w.Name())
+
+		Copy("testdata/input.txt", w.Name(), 100, 1000)
+		wStat, err := w.Stat()
+		if err != nil {
+			log.Fatal(err)
+		}
+		oStat, err := o.Stat()
+		if err != nil {
+			log.Fatal(err)
+		}
+		require.Equal(t, wStat.Size(), oStat.Size())
+	})
+	t.Run("test data out offset6000 limit1000", func(t *testing.T) {
+		w, err := os.CreateTemp("testdata", "out")
+		if err != nil {
+			log.Fatal(err)
+		}
+		o, err := os.Open("testdata/out_offset6000_limit1000.txt")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer os.Remove(w.Name())
+
+		Copy("testdata/input.txt", w.Name(), 6000, 1000)
+		wStat, err := w.Stat()
+		if err != nil {
+			log.Fatal(err)
+		}
+		oStat, err := o.Stat()
+		if err != nil {
+			log.Fatal(err)
+		}
+		require.Equal(t, wStat.Size(), oStat.Size())
+	})
 }
