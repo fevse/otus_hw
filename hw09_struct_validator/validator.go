53dee51ae7a592c4cc35f79ba52dc0bb73bc51d6
fevse <fedulov.v.s@gmail.com>
2024-02-19 20:35:54 +0400

    HW09 is completed

--- hw09_struct_validator/validator.go
+++ hw09_struct_validator/validator.go
@@ -1,5 +1,29 @@
 package hw09structvalidator
 
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+var (
+	ErrorUnsupportedType = errors.New("unsupported type")
+	ErrorRegexp          = errors.New("regexp error")
+	ErrorStrconvAtoi     = errors.New("strconvAtoi error")
+)
+
+var (
+	ErrorIntIn        = errors.New("int in err")
+	ErrorIntMax       = errors.New("int max err")
+	ErrorIntMin       = errors.New("int min err")
+	ErrorStringLen    = errors.New("string len err")
+	ErrorStringIn     = errors.New("string in err")
+	ErrorStringRegexp = errors.New("string regexp err")
+)
+
 type ValidationError struct {
 	Field string
 	Err   error
@@ -8,10 +32,167 @@ type ValidationError struct {
 type ValidationErrors []ValidationError
 
 func (v ValidationErrors) Error() string {
-	panic("implement me")
+	err := ""
+	for _, e := range v {
+		err += fmt.Sprintf("Field: %v, error: %v", e.Field, e.Err)
+	}
+	return err
 }
 
 func Validate(v interface{}) error {
-	// Place your code here.
+	vv := reflect.TypeOf(v)
+	if vv.Kind().String() != "struct" {
+		return ErrorUnsupportedType
+	}
+	p := reflect.ValueOf(v)
+	var ve ValidationErrors
+	for i := 0; i < vv.NumField(); i++ {
+		field := vv.Field(i)
+		value := p.Field(i)
+		if validate, ok := field.Tag.Lookup("validate"); ok {
+			x := strings.Split(validate, "|")
+			for i := range x {
+				s := strings.Split(x[i], ":")
+				switch field.Type.String() {
+				case "[]string":
+					for j := 0; j < value.Len(); j++ {
+						val := value.Index(j).String()
+						switch s[0] {
+						case "len":
+							l, err := strconv.Atoi(s[1])
+							if err != nil {
+								return ErrorStrconvAtoi
+							}
+							if len(val) != l {
+								ve = append(ve, ValidationError{field.Name, ErrorStringLen})
+							}
+						case "in":
+							in := strings.Split(s[1], ",")
+							ok := false
+							for _, j := range in {
+								if j == val {
+									ok = true
+								}
+							}
+							if !ok {
+								ve = append(ve, ValidationError{field.Name, ErrorStringIn})
+							}
+						case "regexp":
+							re, err := regexp.Compile(s[1])
+							if err != nil {
+								return ErrorRegexp
+							}
+							if !re.MatchString(val) {
+								ve = append(ve, ValidationError{field.Name, ErrorStringRegexp})
+							}
+						}
+					}
+				case "string":
+					val := value.String()
+					switch s[0] {
+					case "len":
+						l, err := strconv.Atoi(s[1])
+						if err != nil {
+							return ErrorStrconvAtoi
+						}
+						if len(val) != l {
+							ve = append(ve, ValidationError{field.Name, ErrorStringLen})
+						}
+					case "in":
+						in := strings.Split(s[1], ",")
+						ok := false
+						for _, j := range in {
+							if j == val {
+								ok = true
+							}
+						}
+						if !ok {
+							ve = append(ve, ValidationError{field.Name, ErrorStringIn})
+						}
+					case "regexp":
+						re, err := regexp.Compile(s[1])
+						if err != nil {
+							return ErrorRegexp
+						}
+						if !re.MatchString(val) {
+							ve = append(ve, ValidationError{field.Name, ErrorStringRegexp})
+						}
+					}
+				case "[]int":
+					for j := 0; j < value.Len(); j++ {
+						val := value.Int()
+						switch s[0] {
+						case "in":
+							in := strings.Split(s[1], ",")
+							ok := false
+							for _, j := range in {
+								if j == strconv.Itoa(int(val)) {
+									ok = true
+								}
+							}
+							if !ok {
+								ve = append(ve, ValidationError{field.Name, ErrorIntIn})
+							}
+						case "max":
+							max, err := strconv.Atoi(s[1])
+							if err != nil {
+								fmt.Println(s[1])
+								return ErrorStrconvAtoi
+							}
+							if val > int64(max) {
+								ve = append(ve, ValidationError{field.Name, ErrorIntMax})
+							}
+						case "min":
+							min, err := strconv.Atoi(s[1])
+							if err != nil {
+								fmt.Println(s[1])
+								return ErrorStrconvAtoi
+							}
+							if val < int64(min) {
+								ve = append(ve, ValidationError{field.Name, ErrorIntMin})
+							}
+						}
+					}
+				case "int":
+					val := value.Int()
+					switch s[0] {
+					case "in":
+						in := strings.Split(s[1], ",")
+						ok := false
+						for _, j := range in {
+							if j == strconv.Itoa(int(val)) {
+								ok = true
+							}
+						}
+						if !ok {
+							ve = append(ve, ValidationError{field.Name, ErrorIntIn})
+						}
+					case "max":
+						max, err := strconv.Atoi(s[1])
+						if err != nil {
+							fmt.Println(s[1])
+							return ErrorStrconvAtoi
+						}
+						if val > int64(max) {
+							ve = append(ve, ValidationError{field.Name, ErrorIntMax})
+						}
+					case "min":
+						min, err := strconv.Atoi(s[1])
+						if err != nil {
+							fmt.Println(s[1])
+							return ErrorStrconvAtoi
+						}
+						if val < int64(min) {
+							ve = append(ve, ValidationError{field.Name, ErrorIntMin})
+						}
+					}
+				}
+			}
+		}
+	}
+
+	if len(ve) > 0 {
+		return ve
+	}
 	return nil
 }
--- hw09_struct_validator/validator_test.go
+++ hw09_struct_validator/validator_test.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 type UserRole string
@@ -43,6 +45,56 @@ func TestValidate(t *testing.T) {
 	}{
 		{
 			// Place your code here.
+			Validate(100),
+			ErrorUnsupportedType,
+		},
+		{
+			Validate(User{
+				ID:    "12345678901234567890123456789012345", // 35 - validation error
+				Name:  "DefaultUser",
+				Age:   55,              // validation error
+				Email: "unknown email", // validation error
+				Role:  "user",
+				Phones: []string{
+					"2128505",
+					"2128506",
+					"2128507",
+				}, // validation error
+			}),
+			ValidationErrors{
+				ValidationError{"ID", ErrorStringLen},
+				ValidationError{"Age", ErrorIntMax},
+				ValidationError{"Email", ErrorStringRegexp},
+				ValidationError{"Phones", ErrorStringLen},
+				ValidationError{"Phones", ErrorStringLen},
+				ValidationError{"Phones", ErrorStringLen},
+			},
+		},
+		{
+			Validate(Response{
+				Code: 200,
+				Body: "hello",
+			}),
+			nil,
+		},
+		{
+			Validate(Response{
+				Code: 101,
+				Body: "oh, no",
+			}),
+			ValidationErrors{ValidationError{"Code", ErrorIntIn}},
+		},
+		{
+			Validate(App{
+				Version: "hello",
+			}),
+			nil,
+		},
+		{
+			Validate(App{
+				Version: "goodbye",
+			}),
+			ValidationErrors{ValidationError{"Version", ErrorStringLen}},
 		},
 		// ...
 		// Place your code here.
@@ -54,6 +106,10 @@ func TestValidate(t *testing.T) {
 			t.Parallel()
 
 			// Place your code here.
+			e := tt.in
+			if e != nil {
+				require.Equal(t, e, tt.expectedErr)
+			}
 			_ = tt
 		})
 	}
