444ba59e1b7ad23a497994ee275639eb90d2ee3b
fevse <fedulov.v.s@gmail.com>
2024-03-16 15:38:45 +0400

    update config

--- hw12_13_14_15_calendar/cmd/calendar/config.go
+++ hw12_13_14_15_calendar/cmd/calendar/config.go
@@ -1,11 +1,19 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/BurntSushi/toml"
+)
+
 // При желании конфигурацию можно вынести в internal/config.
 // Организация конфига в main принуждает нас сужать API компонентов, использовать
 // при их конструировании только необходимые параметры, а также уменьшает вероятность циклической зависимости.
 type Config struct {
-	Logger LoggerConf
 	// TODO
+	Logger 		LoggerConf
+	HTTPServer 	HTTPconf
+	DB 			DBConf
 }
 
 type LoggerConf struct {
@@ -13,8 +21,23 @@ type LoggerConf struct {
 	// TODO
 }
 
-func NewConfig() Config {
-	return Config{}
+type HTTPconf struct {
+	Host string
+	Port string
+}
+
+type DBConf struct {
+	Type string
+	DSN string
+}
+
+func NewConfig() (c Config, err error) {
+	_, err = toml.DecodeFile(configFile, &c)
+	if err != nil {
+		return Config{}, err
+	}
+	fmt.Println("Configuration reading completed successfully")
+	return
 }
 
 // TODO
