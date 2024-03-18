59ad8c20d84ee804c88e7939c5820a499c2cb52b
fevse <fedulov.v.s@gmail.com>
2024-03-18 18:45:39 +0400

    update http server

--- hw12_13_14_15_calendar/internal/server/http/server.go
+++ hw12_13_14_15_calendar/internal/server/http/server.go
@@ -2,30 +2,54 @@ package internalhttp
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"net"
+	"net/http"
 )
 
 type Server struct { // TODO
+	Server *http.Server
+	Logger Logger	
+	Handler http.Handler
 }
 
 type Logger interface { // TODO
+	Info(string)
+	Error(string)
 }
 
 type Application interface { // TODO
 }
 
-func NewServer(logger Logger, app Application) *Server {
-	return &Server{}
+func NewServer(logger Logger, app Application, host, port string) *Server {
+	return &Server{
+		Server: &http.Server{
+			Addr: net.JoinHostPort(host, port),
+		},
+		Logger: logger,
+
+	}
 }
 
 func (s *Server) Start(ctx context.Context) error {
 	// TODO
+	mux := http.NewServeMux()
+	mux.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, user!"))
+	})
+	s.Server.Handler = s.loggingMiddleware(mux)
+	s.Logger.Info("Server is running")
+	if err := s.Server.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
+		return fmt.Errorf("server err: %v", err)
+	}
 	<-ctx.Done()
 	return nil
 }
 
 func (s *Server) Stop(ctx context.Context) error {
 	// TODO
-	return nil
+	return s.Server.Shutdown(ctx)
 }
 
 // TODO
